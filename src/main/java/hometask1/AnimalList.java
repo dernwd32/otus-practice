8dc3cc792254190bf5892c24f0f9dd5239b6f7d1
dernwd <dernwd32@gmail.com>
2024-11-21 17:48:28 +0300

    v.0.6 + реализация счётчика животных на hashmap
    v.0.5.1
    + причёсывание кода и оформления в целом
    
    v0.5
    + оптимизация создания новых Animal на switch expression
    + перехват некорректных значений возраста и веса
    + перехват некорректного ввода типа животного
    + перехват некорректного ввода в главном меню
    
    v0.4
    + меню с возможностью ввода разных значений (в т.ч. кириллицей) с общим результатом
    
    v0.3
    + подсчёт животных в list
    + главное меню на do-while вместо отдельных вызовов и System.exit(0)
    
    v0.2.1
    + причёсывание
    - enum работает, меню на switch
    - параметр енама  MenuChoiceEnum.EXIT.getMenuOption() в константу не пихается, т.ч. по параметру меню пока не работает

--- src/main/java/hometask1/AnimalList.java
+++ src/main/java/hometask1/AnimalList.java
@@ -1,6 +1,8 @@
 package hometask1;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AnimalList {
 
@@ -23,24 +25,39 @@ public class AnimalList {
 
     public static String countAnimals() {
 
-        ArrayList<String> animalTypes = new ArrayList<>();
-        listAnimals.forEach(x -> animalTypes.add(x.getClass().toString()) );
+        Map<String, Integer> animalTypes = new HashMap<>();
 
-      //  System.out.println(animalTypes.toString());
-        int cats=0, dogs=0, ducks=0;
-        for (String elem:animalTypes) {
-            //String elemTrash = "class " + System.class.getPackage().getName() + ".";
-            //String cleanedElem = elem.replaceFirst(elemTrash, elem);
-            String[] elems = elem.split("\\.");
-            String cleaned = elems[elems.length-1];
-            //System.out.println(cleaned);
-            switch (cleaned) {
-                case "Cat": cats++; break;
-                case "Dog": dogs++; break;
-                case "Duck": ducks++; break;
-            }
-        }
-        return "\nВсего животных: " + (listAnimals.size()) + " | Кошек: " + cats  + " | Собак: " + dogs + " | Уток: " + ducks + "\n";
+        listAnimals.forEach(
+                x -> {
+                    String[] elems = x.getClass().toString().split("\\.");
+                    String thisClass =  elems[elems.length-1];
+                    if(!animalTypes.containsKey(thisClass))
+                        animalTypes.put(thisClass, 1);
+                    else
+                        animalTypes.put(thisClass, animalTypes.get(thisClass) + 1);
+                }
+        );
+
+        return "\n| Всего животных: " + (listAnimals.size()) + " | Кошек: " + animalTypes.get("Cat")  +
+                " | Собак: " + animalTypes.get("Dog") + " | Уток: " + animalTypes.get("Duck") + " |\n";
+
+//        ArrayList<String> animalTypes = new ArrayList<>();
+//        listAnimals.forEach(x -> animalTypes.add(x.getClass().toString()) );
+//      //  System.out.println(animalTypes.toString());
+//        int cats=0, dogs=0, ducks=0;
+//        for (String elem:animalTypes) {
+//            //String elemTrash = "class " + System.class.getPackage().getName() + ".";
+//            //String cleanedElem = elem.replaceFirst(elemTrash, elem);
+//            String[] elems = elem.split("\\.");
+//            String cleaned = elems[elems.length-1];
+//            //System.out.println(cleaned);
+//            switch (cleaned) {
+//                case "Cat": cats++; break;
+//                case "Dog": dogs++; break;
+//                case "Duck": ducks++; break;
+//            }
+//        }
+       // return "\nВсего животных: " + (listAnimals.size()) + " | Кошек: " + cats  + " | Собак: " + dogs + " | Уток: " + ducks + "\n";
 
     }
 }
